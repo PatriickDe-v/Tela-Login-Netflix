8445a622f3552e5217290351473a1a8605a5490d
PatriickDe-v <109480086+PatriickDe-v@users.noreply.github.com>
2022-08-30 00:15:42 -0300

    Desenvolvido a parte mobile first

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let inf = document.getElementById ('infgoogle')
+function saibamais (){
+    if (inf.style.display == 'none'){
+        inf.style.display == 'block'
+    } else {
+        inf.style.display = 'none'
+    }
+}
