64330c91eb1a0ccd3255000c2529ea01fd0ddee8
PatriickDe-v <109480086+PatriickDe-v@users.noreply.github.com>
2022-08-30 21:08:42 -0300

    List.style

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 
-let inf = document.getElementById ('infgoogle')
-function saibamais (){
-    if (inf.style.display == 'none'){
-        inf.style.display == 'block'
-    } else {
-        inf.style.display = 'none'
-    }
-}
